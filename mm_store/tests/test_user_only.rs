28b348d8c22ee80a0f97a13743dbf5f7978b6221
Charlie Barto <barto.charlie@gmail.com>
2023-04-10 17:48:33 -0700

    cargo fix

--- mm_store/tests/test_user_only.rs
+++ mm_store/tests/test_user_only.rs
@@ -2,12 +2,12 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-only
 
-use byteorder::BE;
+
 use camino::{Utf8PathBuf};
-use cap_std::{ambient_authority, fs::Dir};
-use mm_store::*;
-use std::{io};
-use zvariant::{EncodingContext};
+
+
+
+
 fn datapath() -> Utf8PathBuf {
     let mut datapath = Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     datapath.push("testdata");
